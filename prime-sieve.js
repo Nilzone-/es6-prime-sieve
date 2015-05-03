3c99f436787216c943fff4533cc33782040879d1
Andy Kitson <andrew.kitson@gmail.com>
2015-05-03 10:55:37 -0700

    Switch 'for x of' loops to yield* for recursion

--- prime-sieve.js
+++ prime-sieve.js
@@ -1,3 +1,5 @@
+// TODO: memoize! Also
+
 // We'll start out with an iterator on all integers starting with n.
 var numbers = function* (n) {
   n = n || 0;
@@ -7,7 +9,7 @@ var numbers = function* (n) {
 };
 
 var filterIter = function* (iter, test) {
-  for (var val of iter) { // do an already existing iterator??
+  for (var val of iter) {
     if (test(val)) {
       yield val;
     }
@@ -15,33 +17,31 @@ var filterIter = function* (iter, test) {
 };
 
 // When we find a prime, we filter out multiples of that prime, so let's
-// have a function that'll let us test whether x is a multiple of y.
+// have a function that'll let us test whether x is a multiple of y and
+// one that handles "not"
 var multipleOf = function (x) {
   return function (n) {
     return n % x === 0;
-  }
+  };
 };
-
-// Useful in composing filters
 var not = function (fn) {
   return function () {
     return !fn.apply(this, arguments);
   };
 };
 
-// Ah, Scheme, how I miss you...
+// add a value to the front of a stream
 var consStream = function* (first, rest) {
   yield first;
-  for (var x of rest) {
-    yield x;
-  }
+  yield* consStream(rest.next().value, rest);
 };
 
+// And finally, our sieve!
 var primes = function* (nums) {
-  nums = nums || numbers(2)
+  nums = nums || numbers(2);
   var n = nums.next().value;
-  for (var x of consStream(n, primes(filterIter(nums, not(multipleOf(n))))))
-  {
-    yield x;
-  }
+  yield* consStream(n, primes(filterIter(nums, not(multipleOf(n)))));
 };
+
+
+
